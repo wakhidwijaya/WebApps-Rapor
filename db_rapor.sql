94ae06aa93de75a864741b825e3e55061d1af0e1
wakhidwijaya <wakhidwijaya545@gmail.com>
2019-12-19 17:07:59 +0700

    db update

--- db_rapor.sql
+++ db_rapor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 18, 2019 at 08:10 PM
+-- Generation Time: Dec 19, 2019 at 10:05 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -13,6 +13,135 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_rapor`
 --
+CREATE DATABASE IF NOT EXISTS `db_rapor` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `db_rapor`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_guru`
+--
+
+CREATE TABLE `tb_guru` (
+  `id_guru` int(11) NOT NULL,
+  `nip` int(15) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jenis_kelamin` int(1) NOT NULL,
+  `tempat_lahir` varchar(50) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `status` int(1) NOT NULL,
+  `mapel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_guru`
+--
+
+INSERT INTO `tb_guru` (`id_guru`, `nip`, `nama`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `status`, `mapel`) VALUES
+(1, 2019, 'Sutisna', 1, 'Bantul', '1972-03-21', 'Bantul', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `kelas` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_kelas`
+--
+
+INSERT INTO `tb_kelas` (`id_kelas`, `kelas`) VALUES
+(1, '7A'),
+(5, '7B'),
+(6, '7C'),
+(7, '7D'),
+(8, '8A'),
+(9, '8B'),
+(10, '8C'),
+(11, '8D'),
+(12, '9A'),
+(13, '9B'),
+(14, '9C'),
+(15, '9D');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mapel`
+--
+
+CREATE TABLE `tb_mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `kode_mapel` int(4) NOT NULL,
+  `mapel` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_mapel`
+--
+
+INSERT INTO `tb_mapel` (`id_mapel`, `kode_mapel`, `mapel`) VALUES
+(1, 1, 'Bahasa Indonesia'),
+(2, 2, 'Matematika'),
+(31, 3, 'Bahasa Inggris'),
+(32, 4, 'Ilmu Pengetahuan Alam'),
+(33, 5, 'Ilmu Pengetahuan Sosial'),
+(34, 6, 'Pendidikan Agama'),
+(35, 7, 'Penjaskes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_nilai`
+--
+
+CREATE TABLE `tb_nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_guru` int(11) NOT NULL,
+  `id_mapel` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL,
+  `kelas` int(11) NOT NULL,
+  `semester` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_nilai`
+--
+
+INSERT INTO `tb_nilai` (`id_nilai`, `id_siswa`, `id_guru`, `id_mapel`, `nilai`, `kelas`, `semester`) VALUES
+(1, 1530, 1, 1, 75, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+
+CREATE TABLE `tb_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kelas` int(5) NOT NULL,
+  `jenis_kelamin` int(1) NOT NULL,
+  `tempat_lahir` varchar(50) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `wali_murid` int(10) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_siswa`
+--
+
+INSERT INTO `tb_siswa` (`nis`, `nama`, `kelas`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `wali_murid`, `status`) VALUES
+(1530, 'Wakhid Wijaya', 1, 1, 'Bantul', '1998-03-24', 'Mandungan RT 03 Srimartani Piyungan Bantul Yogyakarta', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -41,6 +170,44 @@ INSERT INTO `tb_user` (`id_user`, `username`, `password`, `email`, `status`) VAL
 --
 
 --
+-- Indexes for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  ADD PRIMARY KEY (`nip`) USING BTREE,
+  ADD UNIQUE KEY `id_guru` (`id_guru`) USING BTREE,
+  ADD KEY `mapel` (`mapel`);
+
+--
+-- Indexes for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `tb_mapel`
+--
+ALTER TABLE `tb_mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- Indexes for table `tb_nilai`
+--
+ALTER TABLE `tb_nilai`
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `id_siswa` (`id_siswa`),
+  ADD KEY `id_guru` (`id_guru`),
+  ADD KEY `id_mapel` (`id_mapel`),
+  ADD KEY `kelas` (`kelas`);
+
+--
+-- Indexes for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD PRIMARY KEY (`nis`),
+  ADD KEY `kelas` (`kelas`),
+  ADD KEY `tb_siswa_ibfk_2` (`wali_murid`);
+
+--
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
@@ -52,7 +219,222 @@ ALTER TABLE `tb_user`
 --
 
 --
+-- AUTO_INCREMENT for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `tb_mapel`
+--
+ALTER TABLE `tb_mapel`
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `tb_nilai`
+--
+ALTER TABLE `tb_nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  ADD CONSTRAINT `tb_guru_ibfk_1` FOREIGN KEY (`mapel`) REFERENCES `tb_mapel` (`id_mapel`);
+
+--
+-- Constraints for table `tb_nilai`
+--
+ALTER TABLE `tb_nilai`
+  ADD CONSTRAINT `tb_nilai_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `tb_siswa` (`nis`),
+  ADD CONSTRAINT `tb_nilai_ibfk_2` FOREIGN KEY (`id_guru`) REFERENCES `tb_guru` (`id_guru`),
+  ADD CONSTRAINT `tb_nilai_ibfk_3` FOREIGN KEY (`id_mapel`) REFERENCES `tb_mapel` (`id_mapel`),
+  ADD CONSTRAINT `tb_nilai_ibfk_4` FOREIGN KEY (`kelas`) REFERENCES `tb_kelas` (`id_kelas`);
+
+--
+-- Constraints for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD CONSTRAINT `tb_siswa_ibfk_1` FOREIGN KEY (`kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `tb_siswa_ibfk_2` FOREIGN KEY (`wali_murid`) REFERENCES `tb_guru` (`id_guru`);
+--
+-- Database: `pmi`
+--
+CREATE DATABASE IF NOT EXISTS `pmi` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pmi`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Blood Stock`
+--
+
+CREATE TABLE `Blood Stock` (
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_stocks`
+--
+
+CREATE TABLE `blood_stocks` (
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `blood_stocks`
+--
+
+INSERT INTO `blood_stocks` (`created_at`, `updated_at`, `id`, `name`, `stock`) VALUES
+('2019-12-10 09:20:11', '2019-12-10 09:20:11', 1, 'A+', 10),
+('2019-12-10 09:20:22', '2019-12-10 09:20:22', 2, 'A-', 10),
+('2019-12-10 09:20:30', '2019-12-10 09:20:30', 3, 'B-', 10),
+('2019-12-10 09:20:33', '2019-12-10 09:20:33', 4, 'B+', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(28, '2019_12_06_081232_articles', 1),
+(29, '2019_12_10_150254_Blood Stock', 1),
+(30, '2019_12_10_150320_blood_stocks', 1),
+(31, '2019_12_10_155838_Patients', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Patients`
+--
+
+CREATE TABLE `Patients` (
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nik` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `blood` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Blood Stock`
+--
+ALTER TABLE `Blood Stock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `blood_stocks`
+--
+ALTER TABLE `blood_stocks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Patients`
+--
+ALTER TABLE `Patients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Blood Stock`
+--
+ALTER TABLE `Blood Stock`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blood_stocks`
+--
+ALTER TABLE `blood_stocks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `Patients`
+--
+ALTER TABLE `Patients`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
