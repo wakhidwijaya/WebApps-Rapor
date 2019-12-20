27e10fa24b7797e581c3eae61f06432ddf93864d
MN08 <mnurhuda009@gamil.com>
2019-12-20 19:02:34 +0700

    change bootstrap to 4

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,28 +1,32 @@
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Rapor Online</title>
-		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<!--		<link rel="shortcut icon" href="--><?php //echo base_url('assets/img/logo.ico');?><!--" type="image/x-icon">-->
-		<link href="<?php echo base_url('assets/plugins/touchspin/jquery.bootstrap-touchspin.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/bootstrap/3.3.6/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/theme/css/AdminLTE.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/dataTables/dataTables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/Font-Awesome/3.2.0/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/Font-Awesome/4.3.0/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/date-range-picker/daterangepicker.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/select-2/select2.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/css/font-awesome-animation.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/plugins/ionicons/css/ionicons.min.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/theme/css/skins/skin-black.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet" type="text/css" />
-	</head>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Rapor Online</title>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<!--		<link rel="shortcut icon" href="--><?php //echo base_url('assets/img/logo.ico');
+													?>
+	<!--" type="image/x-icon">-->
+	<link href="<?php echo base_url('assets/plugins/touchspin/jquery.bootstrap-touchspin.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/theme/css/AdminLTE.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/dataTables/dataTables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/Font-Awesome/3.2.0/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/Font-Awesome/4.3.0/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/date-range-picker/daterangepicker.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/select-2/select2.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/css/font-awesome-animation.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/plugins/ionicons/css/ionicons.min.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/theme/css/skins/skin-black.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet" type="text/css" />
+</head>
+
 <body class="hold-transition skin-black sidebar-mini">
 	<header class="main-header">
 		<a href="<?php echo base_url() ?>" class="logo">
 			<span class="logo-mini"><img style="max-height: 40px; padding-top:5px" src="<?php echo base_url('assets/img/logo_2.png') ?>"> </span>
-			<span class="logo-lg"><img style="padding-top: 7px; max-height: 40px" src="<?php echo base_url('assets/img/logo_1.png'); ?>" ></span>
+			<span class="logo-lg"><img style="padding-top: 7px; max-height: 40px" src="<?php echo base_url('assets/img/logo_1.png'); ?>"></span>
 		</a>
 
 		<nav class="navbar navbar-static-top" role="navigation">
@@ -33,10 +37,12 @@
 			<div class="navbar-custom-menu">
 				<ul class="nav navbar-nav">
 					<li>
-<!--						<a href="--><?//=$logout;?><!--">-->
+						<!--						<a href="--><? //=$logout;
+																?>
+						<!--">-->
 						<i class="fa fa-sign-out"></i> Logout
-					</a>
-			   </li>
+						</a>
+					</li>
 				</ul>
 			</div>
 		</nav>
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,27 +1,30 @@
-<php
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" >
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/plugins/bootstrap/3.3.6/css/bootstrap.min.css")?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/plugins/Font-Awesome/4.3.0/css/font-awesome.min.css")?>">
-	<title>Rapor - Online</title>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <h1>HELLO</h1>
-            <form method="post" action="<?php echo base_url("login")?>">
-            <label>Username</label>
-            <input name="username" type="text" required>
-            <input name="password" type="password" required>
-            <input type="submit">
-            </form>
-        </div>
-    </div>
+<php <!DOCTYPE html>
+    <html lang="en">
 
-    <script src="<?php echo base_url("assets/js/jquery-2.2.0.min.js")?>"></script>
-    <script src="<?php echo base_url("assets/plugins/bootstrap/3.3.6/js/bootstrap.min.js")?>"></script>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('./assets/plugins/bootstrap/css/bootstrap.min.css'); ?>"> -->
+        <link rel="stylesheet" href="<?php echo base_url('./assets/plugins/bootstrap/css/bootstrap.min.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('./assets/plugins/Font-Awesome/4.3.0/css/font-awesome.min.css'); ?>">
+        <title>Rapor - Online</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1>HELLO</h1>
+                <form method="post" action="<?php echo base_url("login") ?>">
+                    <label>Username</label>
+                    <input name="username" type="text" required>
+                    <input name="password" type="password" required>
+                    <input type="submit">
+                </form>
+            </div>
+        </div>
+
+        <script src="<?php echo base_url('./assets/js/jquery-2.2.0.min.js'); ?>"></script>
+        <script src="<?php echo base_url('./assets/plugins/bootstrap/js/bootstrap.min.js'); ?>"></script>
+    </body>
+
+    </html>
