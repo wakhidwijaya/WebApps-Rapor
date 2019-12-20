4afef1ace79f3ba5603c6e9d8e25d7ac0a98be8b
MN08 <mnurhuda009@gamil.com>
2019-12-20 22:07:48 +0700

    add admin lte

--- application/controllers/guru/C_Guru.php
+++ application/controllers/guru/C_Guru.php
@@ -1,25 +1,26 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class C_Guru extends CI_Controller {
-    function __construct(){
+class C_Guru extends CI_Controller
+{
+    function __construct()
+    {
         parent::__construct();
-        $this -> load -> model('guru/M_Guru');
+        $this->load->model('guru/M_Guru');
 
-        if($this->session->userdata('status') != "2"){
+        if ($this->session->userdata('status') != "2") {
             $url = base_url();
             redirect($url);
         }
     }
 
-	public function index()
-	{
+    public function index()
+    {
         $where =  $this->session->userdata('username');
         $data['siswa'] = $this->M_Guru->siswa($where);
-//        $this->load->view('layout/header');
-//        $this->load->view('layout/sidebar');
+        $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
         $this->load->view('guru/v_dashboard', $data);
-//        $this->load->view('layout/footer');
-
+        $this->load->view('layout/footer');
     }
 }
