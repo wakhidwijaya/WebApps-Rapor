0b618ae5990d4a51efe4f251673e1ad085176b3f
wakhidwijaya <wakhidwijaya545@gmail.com>
2019-12-22 03:40:18 +0700

    pull

--- application/models/guru/M_Guru.php
+++ application/models/guru/M_Guru.php
@@ -11,12 +11,11 @@ class M_Guru extends CI_Model
         return $query->result_array();
     }
     function nilai($where){
-        $this->db->select('tn.nilai, tm.mapel, ts.nama');
+        $this->db->select('*');
         $this->db->from('tb_nilai tn');
         $this->db->from('tb_guru tg');
         $this->db->where('tg.nip', $where);
         $this->db->join('tb_siswa ts', 'tn.id_siswa = ts.nis');
-        $this->db->join('tb_mapel tm', 'tn.id_mapel = tm.id_mapel');
         $query = $this -> db -> get();
         return $query->result_array();
     }
