9077b9481e6c83303a70ca12862cd0dd07910607
wakhidwijaya <wakhidwijaya545@gmail.com>
2019-12-22 22:15:24 +0700

    aku lali benake opo wae

--- application/models/guru/M_Guru.php
+++ application/models/guru/M_Guru.php
@@ -11,13 +11,13 @@ class M_Guru extends CI_Model
         return $query->result_array();
     }
     function nilai($where){
-        $this->db->select('*');
-        $this->db->from('tb_nilai tn');
-        $this->db->join('tb_siswa ts', 'tn.id_siswa = ts.nis');
-        $this->db->join('tb_guru tg', 'tn.id_guru = tg.id_guru');
-        $this->db->join('tb_mapel tm', 'tg.id_mapel = tm.id_mapel');
-        $this->db->where('tg.nip', $where);
-        $query = $this -> db -> get();
+        $query = $this->db->query('SELECT ts.nis, ts.nama as nama, tk.kelas, tn.semester, tn.nilai, tm.mapel
+            FROM tb_nilai AS tn 
+            JOIN tb_siswa AS ts ON tn.id_siswa = ts.nis 
+            JOIN tb_guru as tg ON tn.id_guru=tg.id_guru
+            JOIN tb_mapel as tm ON tg.id_mapel = tm.id_mapel
+            JOIN tb_kelas as tk ON ts.kelas = tk.id_kelas
+            WHERE tg.nip = 2019');
         return $query->result_array();
     }
 }
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,9 +1,9 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="<?php echo base_url()?>" class="brand-link">
-      <img src="<?php echo base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="Smanindo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt=Spenindo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-bold">SMA N Indonesia</span>
+      <span class="brand-text font-weight-bold">SMP N Indonesia</span>
     </a>
 
     <div class="sidebar">
@@ -13,32 +13,32 @@
         </div>
         <div class="info">
           <a href="<?php echo base_url()?>" class="d-block"><?php echo $this->session->userdata('nama')." - ". $this->session->userdata('username')?></a>
-            <small class=" text-success"><i style="color: green" class="fa fa-circle fa-1x disabled" ></i> Online</small>
+            <small class="text-light"><i class="text-success fa fa-circle fa-1x disabled" ></i> Online</small>
         </div>
       </div>
 
       <nav id="guru" class="mt-2 <?php if($this->session->userdata('status')==1){echo 'd-none';} ?>">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="<?php echo base_url()?>" class="nav-link active">
+                <a href="<?php echo base_url()?>" class="<?=($this->uri->segment(2)===null)?'active':''?> nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>Dashboard</p>
                 </a>
             </li>
             <li class="nav-item">
-                <a href="pages/calendar.html" class="nav-link">
+                <a href="pages/calendar.html" class="<?=($this->uri->segment(2)==='jadwal')?'active':''?> nav-link">
                     <i class="nav-icon fas fa-calendar-alt"></i>
                     <p>Jadwal Mengajar</p>
                 </a>
             </li>
             <li class="nav-item">
-                <a href="<?php echo base_url('guru/nilai')?>" class="nav-link">
+                <a href="<?php echo base_url('guru/nilai')?>" class="<?=($this->uri->segment(2)==='nilai')?'active':''?> nav-link">
                     <i class="nav-icon fas fa-list-ul"></i>
                     <p>Nilai Siswa</p>
                 </a>
             </li>
             <li class="nav-item">
-                <a href="pages/calendar.html" class="nav-link">
+                <a href="pages/calendar.html" class="<?=($this->uri->segment(2)==='wali')?'active':''?> nav-link">
                     <i class="nav-icon fas fa-users"></i>
                     <p>Wali Kelas</p>
                 </a>
@@ -99,8 +99,8 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url()?>">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item"><a href="<?php echo base_url($this->uri->segment(1))?>">Home</a></li>
+              <li class="breadcrumb-item active"><?php echo $this->uri->segment(2)?></li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
