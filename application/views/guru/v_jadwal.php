53caaf30b21612f360b263bfa0f7c4cefd54d357
wakhidwijaya <wakhidwijaya545@gmail.com>
2020-01-02 15:08:56 +0700

    error message login

---
+++ application/views/guru/v_jadwal.php
@@ -0,0 +1,13 @@
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="info-box">
+                Kelas yang bisa diambil = <?php echo $slot?>
+                <a href="<?php echo base_url('guru/rombel/ambilkelas')?>">
+                    Ambil Kelas
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
